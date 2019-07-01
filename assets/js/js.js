1baf3ee42a21030792d65a216a6c9f7943f381e5
Kate Zhukel <zhukelkatya@gmail.com>
2019-07-01 07:12:30 +0300

    init project

---
+++ assets/js/js.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.main-slider__container', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: false,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: '.main-slider__slide-btn-next',
+        prevEl: '.main-slider__slide-btn-prev',
+    },
+});
+
+var swiper = new Swiper('.suppliers__container', {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: false,
+    loopFillGroupWithBlank: true,
+    // navigation: {
+    //     nextEl: '.suppliers__slide-btn-next',
+    //     prevEl: '.suppliers__slide-btn-prev',
+    // },
+});
